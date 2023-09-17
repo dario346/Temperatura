0f8cedfd2da3ea7480a55f00684ee5cd4eede254
dario346 <darioradja10@gmail.com>
2023-09-17 21:51:04 +0200

    Fixed database backup

--- temperatura.sql
+++ temperatura.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 17, 2023 at 09:12 PM
+-- Generation Time: Sep 17, 2023 at 09:50 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `temperatura`
 --
+CREATE DATABASE IF NOT EXISTS `temperatura` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `temperatura`;
 
 -- --------------------------------------------------------
 
@@ -38,10 +40,10 @@ CREATE TABLE `linkovi` (
 --
 
 INSERT INTO `linkovi` (`id`, `url`, `description`) VALUES
-(1, 'http://localhost/Temp/?page=read&method=get&date=2023-09-17', 'Get endpoint by Date\r\nMust define page=read&method=get as example above and set date to value you want to search'),
-(2, 'http://localhost/Temp/?page=read&method=get&city=Split', 'Get endpoint by City\r\nMust define page=read&method=get as example above and set Ctiy to value you want to search'),
-(3, 'http://localhost/Temp/?page=read&method=get&city=Split&date=2023-09-18', 'Get endpoint Get by City and Date \r\nMust define page=read&method=get as example above and set date and city to value you want to search'),
-(4, 'http://localhost/Temp/?page=create&method=post&city=Split&date=2023-09-23&temperature=20', 'Post endpoint Post temperature by City and Date \r\nMust define page=create&method=post as example above and set temperature,date and city to value you want to search\r\nIf Temperature for said day already exists, you will not be able to create new record');
+(1, 'http://localhost/Temperatura/?page=read&method=get&date=2023-09-17', 'Get endpoint by Date\r\nMust define page=read&method=get as example above and set date to value you want to search'),
+(2, 'http://localhost/Temperatura/?page=read&method=get&city=Split', 'Get endpoint by City\r\nMust define page=read&method=get as example above and set Ctiy to value you want to search'),
+(3, 'http://localhost/Temperatura/?page=read&method=get&city=Split&date=2023-09-18', 'Get endpoint Get by City and Date \r\nMust define page=read&method=get as example above and set date and city to value you want to search'),
+(4, 'http://localhost/Temperatura/?page=create&method=post&city=Split&date=2023-09-23&temperature=20', 'Post endpoint Post temperature by City and Date \r\nMust define page=create&method=post as example above and set temperature,date and city to value you want to search\r\nIf Temperature for said day already exists, you will not be able to create new record');
 
 -- --------------------------------------------------------
 
@@ -67,7 +69,8 @@ INSERT INTO `temp` (`id`, `city`, `temperature`, `date`) VALUES
 (4, 'Split', 45, '2023-09-18'),
 (5, 'Zagreb', 17, '2023-09-18'),
 (6, 'Zagreb', 22, '2023-09-18'),
-(11, 'Split', 20, '2023-09-23');
+(11, 'Split', 20, '2023-09-23'),
+(12, 'Split', 20, '2023-09-26');
 
 --
 -- Indexes for dumped tables
@@ -99,7 +102,7 @@ ALTER TABLE `linkovi`
 -- AUTO_INCREMENT for table `temp`
 --
 ALTER TABLE `temp`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
